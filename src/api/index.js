dea73b96cdaf9e1ce30dd4e625da62c8a6f4e1b6
makennamartin97 <makennamartin97@yahoo.com>
2020-10-30 19:40:31 -0400

    initial

---
+++ src/api/index.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async(country) => {
+    let changeableUrl = url;
+    if(country){
+        changeableUrl = `${url}/countries/${country}`;
+    }
+    try {
+        const {data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(changeableUrl);
+        // adding second curly brackets inside to structure the data and not have to 
+        // repeat data.etc below
+
+        return { confirmed, recovered, deaths, lastUpdate }
+
+    } catch(error){
+        return error;
+
+    }
+}
+
+// export const fetchDailyData = async () => {
+//     try {
+//       const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
+  
+//       return data.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
+//     } catch (error) {
+//       return error;
+//     }
+//   };
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+
+        return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
+    } catch (error) {
+        return error;
+    }
+};
+
+export const fetchCountries = async () => {
+    try {
+        const {data: {countries}} = await axios.get(`${url}/countries`);
+        return countries.map((country) => country.name);
+
+
+    } catch(error){
+        return error;
+
+    }
+}
